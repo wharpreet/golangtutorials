aebe16d3140d07bf6176c6effd7117a8f7399935
Harpreet Wadhwa <harpreet.wadhwa@belkin.com>
2017-11-26 19:21:56 +0530

    tried package scope and references

---
+++ packages/main/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/wharpreet/golangtutorials/packages/stringutil"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+	fmt.Println(stringutil.MyName)
+}
