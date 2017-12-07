ec13aad3258cdfbdc30d547cc72208a6269b62fb
Harpreet Wadhwa <harpreet.wadhwa@belkin.com>
2017-12-07 18:44:06 +0530

    defined variadic functions

---
+++ functions/variadic_args/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	data := []float64{43, 56, 87, 12, 45, 57}
+	// here "..." after "data" will pass the items of data one by one
+	n := average(data...)
+	fmt.Println(n)
+}
+
+// Veriadic is the last argument of any function where variable type is prefixed with "..."
+// This variable can take zero to any number of arguments
+func average(sf ...float64) float64 {
+	//	fmt.Println(sf)
+	//	fmt.Printf("%T \n", sf)
+	total := 0.0
+	for _, v := range sf {
+		total += v
+	}
+	return total / float64(len(sf))
+}
